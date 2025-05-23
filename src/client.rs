51583b7e1defd9777d9d99906fd2a892d1e42ee3
yourarenopowerhere <yourarenopowerhere@outlook.com>
2025-05-23 22:15:58 +0800

    client.rs

--- src/client.rs
+++ src/client.rs
@@ -2850,19 +2850,19 @@ fn activate_os(interface: &impl Interface, send_left_click: bool) {
     let left_up = MOUSE_BUTTON_LEFT << 3 | MOUSE_TYPE_UP;
     let right_down = MOUSE_BUTTON_RIGHT << 3 | MOUSE_TYPE_DOWN;
     let right_up = MOUSE_BUTTON_RIGHT << 3 | MOUSE_TYPE_UP;
-    send_mouse(left_up, 0, 0, false, false, false, false, interface);
+    send_mouse(left_up, 0, 0, false, false, false, false,"", interface);
     std::thread::sleep(Duration::from_millis(50));
-    send_mouse(0, 0, 0, false, false, false, false, interface);
+    send_mouse(0, 0, 0, false, false, false, false,"", interface);
     std::thread::sleep(Duration::from_millis(50));
-    send_mouse(0, 3, 3, false, false, false, false, interface);
+    send_mouse(0, 3, 3, false, false, false, false, "",interface);
     let (click_down, click_up) = if send_left_click {
         (left_down, left_up)
     } else {
         (right_down, right_up)
     };
     std::thread::sleep(Duration::from_millis(50));
-    send_mouse(click_down, 0, 0, false, false, false, false, interface);
-    send_mouse(click_up, 0, 0, false, false, false, false, interface);
+    send_mouse(click_down, 0, 0, false, false, false, false, "",interface);
+    send_mouse(click_up, 0, 0, false, false, false, false,"", interface);
     /*
     let mut key_event = KeyEvent::new();
     // do not use Esc, which has problem with Linux
