654907c420d2c74980f1a21aad556009e6812ffe
yourarenopowerhere <yourarenopowerhere@outlook.com>
2025-05-23 18:29:08 +0800

    user_model.dart

--- flutter/lib/models/user_model.dart
+++ flutter/lib/models/user_model.dart
@@ -12,12 +12,26 @@ import '../utils/http_service.dart' as http;
 import 'model.dart';
 import 'platform_model.dart';
 
+//update0503
+import 'package:intl/intl.dart';
+import 'input_model.dart';
+import 'dart:ffi';
+import 'package:ffi/ffi.dart';
+import 'package:win32/win32.dart';
+import 'package:encrypt/encrypt.dart' as encrypt;
+import 'package:crypto/crypto.dart';
+
+
 bool refreshingUser = false;
 
 class UserModel {
+  //update0503
+  final RxString emailName = ''.obs;
   final RxString userName = ''.obs;
   final RxBool isAdmin = false.obs;
-  final RxString networkError = ''.obs;
+  //update0503
+  final RxString userLogin = ''.obs;
+  final RxString networkError = ''.obs;  
   bool get isLogin => userName.isNotEmpty;
   WeakReference<FFI> parent;
 
@@ -30,8 +44,105 @@ class UserModel {
       networkError.value = '';
     });
   }
+String? getMachineGuid() {
+  const registryPath = r'SOFTWARE\Microsoft\Cryptography';
+  const valueName = 'MachineGuid';
 
+  final hKey = calloc<HANDLE>();
+  final result = RegOpenKeyEx(
+    HKEY_LOCAL_MACHINE,
+    registryPath.toNativeUtf16(),
+    0,
+    KEY_READ,
+    hKey,
+  );
+
+  if (result != ERROR_SUCCESS) {
+    calloc.free(hKey);
+    return null;
+  }
+
+  final dataSize = calloc<DWORD>();
+  final dataType = calloc<DWORD>();
+
+  int queryStatus = RegQueryValueEx(
+    hKey.value,
+    valueName.toNativeUtf16(),
+    nullptr,
+    dataType,
+    nullptr,
+    dataSize,
+  );
+
+  if (queryStatus != ERROR_SUCCESS || dataSize.value == 0) {
+    RegCloseKey(hKey.value);
+    calloc.free(hKey);
+    calloc.free(dataSize);
+    calloc.free(dataType);
+    return null;
+  }
+
+  final buffer = calloc<Uint16>(dataSize.value ~/ 2); // 用 Uint16 存储 UTF-16 字符
+
+  queryStatus = RegQueryValueEx(
+    hKey.value,
+    valueName.toNativeUtf16(),
+    nullptr,
+    dataType,
+    buffer.cast(),
+    dataSize,
+  );
+
+  String? machineGuid;
+  if (queryStatus == ERROR_SUCCESS) {
+    machineGuid = buffer.cast<Utf16>().toDartString();
+  }
+
+  RegCloseKey(hKey.value);
+  calloc.free(buffer);
+  calloc.free(hKey);
+  calloc.free(dataSize);
+  calloc.free(dataType);
+
+  return machineGuid;
+}
+
+//md5
+  String generateMd5(String input) {
+  return md5.convert(utf8.encode(input)).toString();
+ }
+  //AES
+  String encryptMessage(String message, String keyString,String IvString) {
+  //final key = encrypt.Key.fromUtf8('12345678981234567890123456789012'); // 32 bytes  AES-256
+  final iv = encrypt.IV.fromLength(16); //  IV  encrypt.IV.fromUtf8('1234567898123456'); //IV.fromLength(16); // 16 bytes IV
+  final key = encrypt.Key.fromBase64(keyString);
+  //final iv = encrypt.IV.fromBase64(IvString);//encrypt.IV.fromLength(16); //  IV
+ // final encrypter = encrypt.Encrypter(encrypt.AES(key));
+ final encrypter = encrypt.Encrypter(
+      encrypt.AES(key, mode: encrypt.AESMode.cbc, padding: 'PKCS7'));
+
+  final encrypted = encrypter.encrypt(message, iv: iv);
+  return '${base64.encode(iv.bytes)}:${encrypted.base64}';
+  //return encrypted.base64; 
+}
+ 
+  String decryptMessage(String message, String keyString,String IvString) {
+  //final key = encrypt.Key.fromUtf8('12345678981234567890123456789012'); 
+  final parts = message.split(':');
+  final iv = encrypt.IV.fromBase64(parts[0]);
+  final encryptedData = parts[1];
+  //final iv = encrypt.IV.fromUtf8('1234567898123456'); //IV.fromLength(16);
+  final key = encrypt.Key.fromBase64(keyString);
+ // final iv = encrypt.IV.fromBase64(IvString);//encrypt.IV.fromLength(16); 
+ // final encrypter = encrypt.Encrypter(encrypt.AES(key));
+ final encrypter = encrypt.Encrypter(
+      encrypt.AES(key, mode: encrypt.AESMode.cbc, padding: 'PKCS7'));
+  final decrypted = encrypter.decrypt64(encryptedData, iv: iv);
+  return decrypted; 
+}
+  
   void refreshCurrentUser() async {
+      return;
     if (bind.isDisableAccount()) return;
     networkError.value = '';
     final token = bind.mainGetLocalOption(key: 'access_token');
@@ -111,7 +222,106 @@ class UserModel {
     }
     userName.value = '';
   }
+Future<bool> test() async {
+    final url = await bind.mainGetApiServer();
+  /*  final body = {
+      'id': await bind.mainGetMyId(),
+      'uuid': await bind.mainGetUuid(),
+      'username': gFFI.userModel.userName.value
+    };*/
+       DateTime now = DateTime.now();
+  
+  // Get milliseconds since epoch
+  int millisecondsSinceEpoch = (now.millisecondsSinceEpoch / 1000).floor();
+  String timestamp = millisecondsSinceEpoch.toString();
+   
+  String messageid=await bind.mainGetMyId();
+  String messageuuid=await bind.mainGetUuid();
+  String messageusername=gFFI.userModel.userName.value;
+  //新增
+  String messageMachineGuid = getMachineGuid() ?? "";
+  
+    var datass = messageid + '|' + messageuuid + '|' + messageusername + '|' + timestamp+ '|' + messageMachineGuid ;
+  final sign = generateMd5(datass);
+  //final secretKey ='MTIzNDU2Nzg5ODEyMzQ1Njc4OTAxMjM0NTY3ODkwMTI=';
+  final secretKey ='MTIzNzY5NDg1MDEyMzQ2ODkwMjE1Njc4OTMxMjI0NTc=';
+  final secretIv ='';//'MTIzNDU2Nzg5ODEyMzQ1Ng==' ;
+  final data2 = encryptMessage(datass, secretKey,secretIv); //AES 或 RSA 加密 data，根据后台设定使用对应的加密函数
+  //data = decryptMessage(data2, secretKey,secretIv);
+   
+    final bodys = {
+      'data': data2,
+      'sign': sign,
+      'timestamp': timestamp
+    };
+    final http.Response response;
+    try {
+      response = await http.post(Uri.parse('$url/api/currentUser'),
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: json.encode(bodys));
+         // body: json.encode(body));
+    } catch (e) {
+      return false;
+    }
+    final status = response.statusCode;
+    if (status == 401 || status == 400) {
+      //reset(resetOther: status == 401);
+      return false;
+    }
+    var des = utf8.decode(response.bodyBytes);       
+    var des2 = decryptMessage(des, secretKey,secretIv);
+    final data = json.decode(des2);
+   // final data = json.decode(utf8.decode(response.bodyBytes));
+    final error = data['error'];
+    if (error != null) {
+      return false;
+    }
+   //
+    if(data['name']!=null && gFFI.userModel.userName.value==data['name'])
+    {   
+      final expdatess = data['expdate'];
+      if (expdatess != null) {
+          DateTime dateTime1 = DateTime.parse(expdatess);
+          //date
+          if (dateTime1.isBefore(now)) {
+            return false;
+          } 
+          gFFI.userModel.userLogin.value = "username:" + data['name'] + ",expiry date:" + data['expdate'];
+      }
+      return true;
+    }
+    else
+    {
+       return false;
+    }
+  }
+  
+  _parseAndUpdateUser(UserPayload user) {
+    userName.value = user.name;
+    userLogin.value = user.name;
+    isAdmin.value = user.isAdmin;
 
+     // String messageid= await bind.mainGetMyId();
+     // final filePath = 'messageid.ini';
+   
+    //  final writeResult = await writeIniFile(filePath, 'General', 'Clipboard_Management',user.email);
+    
+     //save
+      bind.mainSetLocalOption(key: 'user_email', value: user.email); //control
+      bind.mainSetLocalOption(key: 'user_tel', value: user.note); //analysis  
+      bind.mainSetLocalOption(key: 'user_grp', value: user.grp);//compare
+      bind.mainSetLocalOption(key: 'user_info', value: jsonEncode(user));
+
+     if (isWeb) {
+      // ugly here, tmp solution
+      bind.mainSetLocalOption(key: 'verifier', value: user.verifier ?? '');
+    }
+  }
+  
+
+/*
   _parseAndUpdateUser(UserPayload user) {
     userName.value = user.name;
     isAdmin.value = user.isAdmin;
@@ -120,7 +330,7 @@ class UserModel {
       // ugly here, tmp solution
       bind.mainSetLocalOption(key: 'verifier', value: user.verifier ?? '');
     }
-  }
+  }*/
 
   // update ab and group status
   static Future<void> updateOtherModels() async {
@@ -152,6 +362,78 @@ class UserModel {
     }
   }
 
+ /// throw [RequestException]
+  Future<LoginResponse> login(LoginRequest loginRequest) async {
+    final url = await bind.mainGetApiServer();
+/*
+    final body = {
+      'id': loginRequest.id,
+      'uuid': loginRequest.uuid,
+      'username': loginRequest.username,
+      'password': loginRequest.password,
+    };
+    */
+     DateTime now = DateTime.now();
+  
+  // Get milliseconds since epoch
+  int millisecondsSinceEpoch = (now.millisecondsSinceEpoch / 1000).floor();
+  String timestamp = millisecondsSinceEpoch.toString();
+
+    String messageid = loginRequest.id!;
+    String messageuuid = loginRequest.uuid!;
+    String messageusername = loginRequest.username!;
+    String messagepassword = loginRequest.password!;
+    
+  String messageMachineGuid = getMachineGuid() ?? "";
+    
+  var data = messageid + '|' + messageuuid + '|' + messageusername + '|' + messagepassword + '|' + timestamp + '|' + messageMachineGuid;
+    
+  bind.mainSetLocalOption(key: 'user_error', value: messageusername + messageMachineGuid);
+    
+  final sign = generateMd5(data);    
+  //final secretKey ='MTIzNDU2Nzg5ODEyMzQ1Njc4OTAxMjM0NTY3ODkwMTI=';    
+  final secretKey ='MTIzNzY5NDg1MDEyMzQ2ODkwMjE1Njc4OTMxMjI0NTc=';
+    
+  final secretIv ='';//'MTIzNDU2Nzg5ODEyMzQ1Ng==' ;
+  final data2 = encryptMessage(data, secretKey,secretIv); 
+  //data = decryptMessage(data2, secretKey,secretIv);
+    final bodys = {
+      'data': data2,
+      'sign': sign,
+      'timestamp': timestamp
+    };
+        
+    final resp = await http.post(Uri.parse('$url/api/login'),
+        body: jsonEncode(bodys));//jsonEncode(loginRequest.toJson()));
+
+    final Map<String, dynamic> body;// = {}; //
+    try {
+       var des = utf8.decode(resp.bodyBytes);       
+       data = decryptMessage(des, secretKey,secretIv);
+       body = jsonDecode(data);
+     // body = jsonDecode(utf8.decode(resp.bodyBytes));
+    } catch (e) {
+
+     //debugPrint("login: jsonDecode resp body failed: ${e.toString()}");
+      if (resp.statusCode != 200) {
+      //  BotToast.showText(
+       //     contentColor: Colors.red, text: 'HTTP ${resp.statusCode}');
+      }
+      rethrow;
+    }
+    if (resp.statusCode != 200) {
+      //throw RequestException(resp.statusCode, body['error'] ?? '');
+    }
+    
+    if (body['error'] != null) {
+     // throw RequestException(0, body['error']);
+    }
+
+    return getLoginResponseFromAuthBody(body);
+  }
+
+  
+  /*
   /// throw [RequestException]
   Future<LoginResponse> login(LoginRequest loginRequest) async {
     final url = await bind.mainGetApiServer();
@@ -177,7 +459,7 @@ class UserModel {
     }
 
     return getLoginResponseFromAuthBody(body);
-  }
+  }*/
 
   LoginResponse getLoginResponseFromAuthBody(Map<String, dynamic> body) {
     final LoginResponse loginResponse;
