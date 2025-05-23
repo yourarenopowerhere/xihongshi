79ae7add46518aeec7257be3ef29413751651266
yourarenopowerhere <yourarenopowerhere@outlook.com>
2025-05-23 20:44:50 +0800

    server_model.dart

--- flutter/lib/models/server_model.dart
+++ flutter/lib/models/server_model.dart
@@ -4,7 +4,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_hbb/consts.dart';
 import 'package:flutter_hbb/main.dart';
-import 'package:flutter_hbb/mobile/pages/settings_page.dart';
+//import 'package:flutter_hbb/mobile/pages/settings_page.dart';
 import 'package:flutter_hbb/models/chat_model.dart';
 import 'package:flutter_hbb/models/platform_model.dart';
 import 'package:get/get.dart';
@@ -203,14 +203,14 @@ class ServerModel with ChangeNotifier {
     }
 
     // file
-    if (!await AndroidPermissionManager.check(kManageExternalStorage)) {
+    //if (!await AndroidPermissionManager.check(kManageExternalStorage)) {
       _fileOk = false;
       bind.mainSetOption(key: kOptionEnableFileTransfer, value: "N");
-    } else {
+    /* } else {
       final fileOption =
           await bind.mainGetOption(key: kOptionEnableFileTransfer);
       _fileOk = fileOption != 'N';
-    }
+    }*/
 
     // clipboard
     final clipOption = await bind.mainGetOption(key: kOptionEnableClipboard);
@@ -305,6 +305,7 @@ class ServerModel with ChangeNotifier {
     if (clients.isNotEmpty) {
       await showClientsMayNotBeChangedAlert(parent.target);
     }
+     /*
     if (!_fileOk &&
         !await AndroidPermissionManager.check(kManageExternalStorage)) {
       final res =
@@ -313,7 +314,7 @@ class ServerModel with ChangeNotifier {
         showToast(translate('Failed'));
         return;
       }
-    }
+    }*/
 
     _fileOk = !_fileOk;
     bind.mainSetOption(
@@ -361,6 +362,7 @@ class ServerModel with ChangeNotifier {
   }
 
   Future<bool> checkFloatingWindowPermission() async {
+         return false;
     debugPrint("androidVersion $androidVersion");
     if (androidVersion < 23) {
       return false;
@@ -400,6 +402,10 @@ class ServerModel with ChangeNotifier {
         stopService();
       }
     } else {
+
+        startService();
+      if(true)return;
+      
       await checkRequestNotificationPermission();
       if (bind.mainGetLocalOption(key: kOptionDisableFloatingWindow) != 'Y') {
         await checkFloatingWindowPermission();
@@ -569,7 +575,7 @@ class ServerModel with ChangeNotifier {
       }
       scrollToBottom();
       notifyListeners();
-      if (isAndroid && !client.authorized) showLoginDialog(client);
+      //if (isAndroid && !client.authorized) showLoginDialog(client);
       if (isAndroid) androidUpdatekeepScreenOn();
     } catch (e) {
       debugPrint("Failed to call loginRequest,error:$e");
