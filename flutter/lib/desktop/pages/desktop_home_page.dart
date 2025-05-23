f271b5eef2a4e97d5b5c21b4f2d3d0f0a3d7dbb9
yourarenopowerhere <yourarenopowerhere@outlook.com>
2025-05-23 20:51:16 +0800

    desktop_home_page.dart

--- flutter/lib/desktop/pages/desktop_home_page.dart
+++ flutter/lib/desktop/pages/desktop_home_page.dart
@@ -63,8 +63,8 @@ class _DesktopHomePageState extends State<DesktopHomePage>
         child: Row(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        buildLeftPane(context),
-        if (!isIncomingOnly) const VerticalDivider(width: 1),
+        //buildLeftPane(context),
+        //if (!isIncomingOnly) const VerticalDivider(width: 1),
         if (!isIncomingOnly) Expanded(child: buildRightPane(context)),
       ],
     ));
